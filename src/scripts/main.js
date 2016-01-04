510fd572c3593ff2500a5315de6f84969de3f5a1
Sean Tracey <seanmtracey@gmail.com>
2016-01-04 17:31:39 +0000

    Initial MVP

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
-const composeButton = document.querySelector('[gh="cm"]');
+const watcher = new MutationObserver(function () {
 
-console.log("composeButton:", composeButton);
+	const messageBodies = [...document.querySelectorAll('div[aria-label="Message Body"]:not([data-ftsig="1"])')];
+
+	messageBodies
+	.filter(message => !message.querySelector('span.data-ftsig-content'))
+	.forEach(function (message) {
+		const signature = document.createElement('div');
+		signature.classList.add('data-ftsig-content');
+		signature.appendChild(
+			document.createRange().createContextualFragment('<hr /><a href="http://ada.is/">ADA!!</a>')
+		);
+		message.appendChild(signature);
+	});
+
+});
+
+
+watcher.observe(document.body, {
+	childList: true
+});
